cb48f3fc3fdfaa9feb0eec4b34e471452cf6084b
liteng <930372551@qq.com>
2017-10-01 21:38:12 +0800

    BList

---
+++ bootstrap-es6/bootstrap-es6/source/Content/BList.js
@@ -0,0 +1,33 @@
+﻿// BList
+
+class BList extends BObject {
+
+    constructor(config) {
+        super(config);
+        this.unstyled = this.config.unstyled || false;
+        this.inline = this.config.inline || false;
+        if (this.inline) {
+            this.prefixHtml = `<ul class="list-inline">`;
+        } else if (this.unstyled) {
+            this.prefixHtml = `<ul class="list-unstyled">`;
+        } else {
+            this.prefixHtml = `<ul>`;
+        }
+        this.suffixHtml = '</ul>';
+    }
+
+    appendChild(item) {
+        if (!item instanceof BListItem) {
+            throw Error('BList: Item is not an instance of BListItem.');
+        }
+        super.appendChild(item);
+    }
+
+    removeChild(item) {
+        if (!item instanceof BListItem) {
+            throw Error('BList: Item is not an instance of BListItem.');
+        }
+        super.removeChild(item);
+    }
+
+}
