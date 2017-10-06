2794f4e90093910b4c8f143e063253f0c3858e6b
liteng <930372551@qq.com>
2017-10-06 10:11:34 +0800

    BBreadcrumb

---
+++ bootstrap-es6/bootstrap-es6/source/Component/BBreadcrumbItem.js
@@ -0,0 +1,27 @@
+﻿// BBreadcrumbItem.js
+
+class BBreadcrumbItem extends BObject {
+
+    constructor(config) {
+        super(config);
+
+        this.text = this.config.text || 'BBreadcrumbItem';
+        this.url = this.config.url || null;
+        this.prefixHtml = '<li class="breadcrumb-item">';
+        if (this.url == null) {
+            this.prefixHtml += this.text;
+        } else {
+            this.prefixHtml += `<a href="${this.url}">${this.text}</a>`;
+        }
+        this.suffixHtml = '</li>';
+    }
+
+    appendChild(item) {
+        throw Error('BBreadcrumbItem: BBreadcrumbItem doesnot support appendChild.');
+    }
+
+    removeChild(item) {
+        throw Error('BBreadcrumbItem: BBreadcrumbItem doesnot support removeChild.');
+    }
+
+}
