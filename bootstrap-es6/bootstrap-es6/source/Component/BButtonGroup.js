2b7c713ea213999a7d8611b4b2c8b37c812d86be
liteng <930372551@qq.com>
2017-10-06 10:33:32 +0800

    BButtonGroup

---
+++ bootstrap-es6/bootstrap-es6/source/Component/BButtonGroup.js
@@ -0,0 +1,12 @@
+﻿// BButtonGroup
+
+class BButtonGroup extends BObject {
+
+    constructor(config) {
+        super(config);
+
+        this.prefixHtml = '<div class="btn-group" role="group">';
+        this.suffixHtml = '</div>';
+    }
+
+}
