2c6bccd803fa2855445162e3abe8cd1a1c8f12ed
liteng <930372551@qq.com>
2017-10-06 10:27:59 +0800

    BButton

---
+++ bootstrap-es6/bootstrap-es6/source/Component/BButtonType.js
@@ -0,0 +1,11 @@
+﻿// BButtonType.js
+
+class BButtonType {
+
+}
+
+BButtonType.primary = 'btn-primary';
+BButtonType.success = 'btn-success';
+BButtonType.danger = 'btn-danger';
+BButtonType.warning = 'btn-warning';
+BButtonType.info = 'btn-info';
