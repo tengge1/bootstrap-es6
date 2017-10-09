4f874df58150766c63b4b6f95a06fc456adcd6aa
liteng <930372551@qq.com>
2017-10-09 13:03:55 +0800

    BHtml

--- bootstrap-es6/bootstrap-es6/source/BHtml.js
+++ bootstrap-es6/bootstrap-es6/source/BHtml.js
@@ -5,25 +5,16 @@ class BHtml extends BObject {
     constructor(config) {
         super(config);
         this.html = this.config.html || 'This is raw html.';
+        this.alias = 'html';
     }
 
     setHtml(html) {
         this.html = html;
+        this.container.innerHTML += this.html;
     }
 
     getHtml() {
         return this.html;
     }
 
-    appendChild(obj) {
-        throw Error('BHtml: BHtml does not support appendChild.');
-    }
-
-    removeChild(obj) {
-        throw Error('BHtml: BHtml does not support removeChild.');
-    }
-
-    toHtml() {
-        return this.html;
-    }
 }
--- bootstrap-es6/bootstrap-es6/source/BObject.js
+++ bootstrap-es6/bootstrap-es6/source/BObject.js
@@ -4,7 +4,7 @@ class BObject {
 
     constructor(config) {
         this.config = config || {};
-        this.container = this.config.container || null;
+        this.container = this.config.container || document.body;
         this.alias = 'object';
     }
 
