363c9ea1522e8fdc7e5f787227b61442d23cb162
liteng <930372551@qq.com>
2017-10-14 07:49:06 +0800

    XAlert

--- bootstrap-es6/bootstrap-es6/dist/js/bootstrap-es6.js
+++ bootstrap-es6/bootstrap-es6/dist/js/bootstrap-es6.js
@@ -298,6 +298,7 @@ class XAlert extends XObject {
     constructor(config) {
         super(config);
         this.cls = this.config.cls || 'alert-primary';
+        this.closable = this.config.closable || true;
         this.children = this.config.children || [];
 
         this.el = {};
@@ -310,11 +311,27 @@ class XAlert extends XObject {
         }
         this.el.alert = document.createElement('div');
         this.el.alert.className = 'alert ' + this.cls;
+        this.el.alert.role = 'alert';
         this.container.appendChild(this.el.alert);
+
+        if (this.closable) {
+            this.el.closeBtn = document.createElement('button');
+            this.el.closeBtn.type = 'button';
+            this.el.closeBtn.className = 'close';
+            this.el.closeBtn.setAttribute('data-dismiss', 'alert');
+            this.el.alert.appendChild(this.el.closeBtn);
+
+            this.el.closeIcon = document.createElement('span');
+            this.el.closeIcon.innerHTML = '&times;';
+            this.el.closeBtn.appendChild(this.el.closeIcon);
+        }
+
+        this.el.alertBody = document.createElement('div');
+        this.el.alert.appendChild(this.el.alertBody);
 
         this.children.forEach((n, i) => {
             var obj = X.create(n);
-            obj.container = this.el.alert;
+            obj.container = this.el.alertBody;
             if (typeof (obj.render) == 'function') {
                 obj.render.call(obj);
             }
