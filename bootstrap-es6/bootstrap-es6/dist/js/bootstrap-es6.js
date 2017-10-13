e6c81b23e086a084d4822042dcb35e3553499962
liteng <930372551@qq.com>
2017-10-13 22:05:36 +0800

    XContainerFluid

--- bootstrap-es6/bootstrap-es6/dist/js/bootstrap-es6.js
+++ bootstrap-es6/bootstrap-es6/dist/js/bootstrap-es6.js
@@ -257,3 +257,39 @@ class XContainer extends XObject {
 
 XType.add('container', XContainer);
 
+// XContainerFluid.js
+
+class XContainerFluid extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.container = this.config.container || document.body;
+        this.children = this.config.children || [];
+
+        this.el = {};
+        this.hasRendered = false;
+    }
+
+    render() {
+        if (this.hasRendered) {
+            return;
+        }
+        this.el.container = document.createElement('div');
+        this.el.container.className = 'container-fluid';
+        this.container.appendChild(this.el.container);
+
+        this.children.forEach((n, i) => {
+            var obj = X.create(n);
+            obj.container = this.el.container;
+            if (typeof (obj.render) == 'function') {
+                obj.render.call(obj);
+            }
+        });
+
+        this.hasRendered = true;
+    }
+
+}
+
+XType.add('containerfluid', XContainerFluid);
+
