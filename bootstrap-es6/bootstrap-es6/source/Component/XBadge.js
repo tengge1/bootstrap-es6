3edeebe1993a1f9223633cfb2c383e7e9d48358a
liteng <930372551@qq.com>
2017-10-14 08:22:58 +0800

    XBadge

---
+++ bootstrap-es6/bootstrap-es6/source/Component/XBadge.js
@@ -0,0 +1,27 @@
+﻿// XBadge.js
+
+class XBadge extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.text = this.config.text || 'Badge';
+        this.cls = this.config.cls || 'badge-primary';
+
+        this.el = {};
+        this.hasRendered = false;
+    }
+
+    render() {
+        if (this.hasRendered) {
+            return;
+        }
+        this.el.badge = document.createElement('span');
+        this.el.badge.innerHTML = this.text;
+        this.el.badge.className = 'badge ' + this.cls;
+        this.container.appendChild(this.el.badge);
+        this.hasRendered = true;
+    }
+
+}
+
+XType.add('badge', XBadge);
