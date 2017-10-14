581b61e80e93b2fb2b7eddf1eb988f401c028f2c
liteng <930372551@qq.com>
2017-10-14 08:09:40 +0800

    XButton

---
+++ bootstrap-es6/bootstrap-es6/source/Component/XButton.js
@@ -0,0 +1,31 @@
+﻿// XButton.js
+
+class XButton extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.text = this.config.text || 'Button';
+        this.cls = this.config.cls || 'btn-primary';
+        this.listeners = this.config.listeners || null;
+
+        this.el = {};
+        this.hasRendered = false;
+    }
+
+    render() {
+        if (this.hasRendered) {
+            return;
+        }
+        this.el.button = document.createElement('button');
+        this.el.button.type = 'button';
+        this.el.button.innerHTML = this.text;
+        this.el.button.className = 'btn ' + this.cls;
+        this.container.appendChild(this.el.button);
+        new XEvent(this.el.button, this.listeners);
+
+        this.hasRendered = true;
+    }
+
+}
+
+XType.add('button', XButton);
