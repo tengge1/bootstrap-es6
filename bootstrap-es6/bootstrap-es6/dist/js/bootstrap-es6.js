0c2afc9fa76ecd2993efc862e664c6cfe639c79c
liteng <930372551@qq.com>
2017-10-15 21:30:08 +0800

    XMedia

--- bootstrap-es6/bootstrap-es6/dist/js/bootstrap-es6.js
+++ bootstrap-es6/bootstrap-es6/dist/js/bootstrap-es6.js
@@ -332,6 +332,52 @@ class XCol extends XObject {
 
 XType.add('col', XCol);
 
+// XMedia.js
+
+class XMedia extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.imgSrc = this.config.imgSrc || null;
+        this.imgAlt = this.config.imgAlt || null;
+        this.children = this.config.children || [];
+
+        this.el = {};
+    }
+
+    render() {
+        if (this.imgSrc == null) {
+            throw 'XMedia: config.imgSrc is undefined.';
+        }
+
+        this.el.media = document.createElement('div');
+        this.el.media.className = 'media';
+        this.container.appendChild(this.el.media);
+
+        this.el.img = document.createElement('img');
+        this.el.img.className = 'd-flex mr-3';
+        this.el.img.src = this.imgSrc;
+        this.el.img.alt = this.imgAlt;
+        this.el.media.appendChild(this.el.img);
+
+        this.el.mediaBody = document.createElement('div');
+        this.el.mediaBody.className = 'media-body';
+        this.el.media.appendChild(this.el.mediaBody);
+
+        this.children.forEach((n, i) => {
+            var obj = X.create(n);
+            obj.container = this.el.mediaBody;
+            if (typeof (obj.render) == 'function') {
+                obj.render.call(obj);
+            }
+        });
+    }
+
+}
+
+XType.add('media', XMedia);
+
+
 // XAlert.js
 
 class XAlert extends XObject {
