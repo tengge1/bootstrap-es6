f3e57eb4bcf61175983509d9ec8e1f2eb0c90f10
liteng <930372551@qq.com>
2017-10-16 13:05:44 +0800

    XDefinitionList

---
+++ bootstrap-es6/bootstrap-es6/source/Content/XDefinitionList.js
@@ -0,0 +1,19 @@
+﻿// DefinitionList.js
+
+class XDefinitionList extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.children = this.config.children || [];
+
+        this.el = {};
+    }
+
+    render() {
+        this.el.dl = document.getElementById('dl');
+
+    }
+
+}
+
+XType.add('definitionlist', XDefinitionList);
