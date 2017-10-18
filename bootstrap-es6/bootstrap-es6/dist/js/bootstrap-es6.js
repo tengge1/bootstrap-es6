77631560c1fd16dff9e2ce0dd82c861f8eed1c3f
liteng <930372551@qq.com>
2017-10-18 08:06:11 +0800

    XButtonGroup

--- bootstrap-es6/bootstrap-es6/dist/js/bootstrap-es6.js
+++ bootstrap-es6/bootstrap-es6/dist/js/bootstrap-es6.js
@@ -994,3 +994,37 @@ class XButton extends XObject {
 
 XType.add('button', XButton);
 
+// XButtonGroup.js
+
+class XButtonGroup extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.children = this.config.children || [];
+
+        this.el = {};
+    }
+
+    render() {
+        this.el.group = document.createElement('div');
+        this.el.group.className = 'btn-group';
+        this.el.group.role = 'group';
+        this.container.appendChild(this.el.group);
+
+        this.children.forEach((n, i) => {
+            var obj = X.create(n);
+            if (!obj instanceof XButton) {
+                throw 'XButtonGroup: obj.children is not a list of XButton.';
+            }
+            obj.container = this.el.group;
+            if (typeof (obj.render) == 'function') {
+                obj.render.call(obj);
+            }
+        });
+
+    }
+
+}
+
+XType.add('buttongroup', XButtonGroup);
+
