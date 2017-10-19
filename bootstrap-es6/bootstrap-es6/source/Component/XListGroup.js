fd94df9139811ed1bbb908c89b7ec26cb0c5e9dd
liteng <930372551@qq.com>
2017-10-19 12:23:51 +0800

    XGroup

---
+++ bootstrap-es6/bootstrap-es6/source/Component/XListGroup.js
@@ -0,0 +1,28 @@
+﻿// XListGroup.js
+
+class XListGroup extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.children = this.config.children || [];
+
+        this.el = {};
+    }
+
+    render() {
+        this.el.ul = document.createElement('ul');
+        this.el.ul.className = 'list-group list-group-flush';
+        this.container.appendChild(this.el.ul);
+
+        this.children.forEach((n, i) => {
+            var obj = X.create(n);
+            obj.container = this.el.ul;
+            if (typeof (obj.render) == 'function') {
+                obj.render.call(obj);
+            }
+        });
+    }
+
+}
+
+XType.add('listgroup', XListGroup);
