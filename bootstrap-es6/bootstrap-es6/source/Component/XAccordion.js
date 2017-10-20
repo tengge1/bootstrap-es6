76b4e8c79d0f1a07fe0932d00f175cce562c82cd
liteng <930372551@qq.com>
2017-10-20 13:02:24 +0800

    XAccordion

---
+++ bootstrap-es6/bootstrap-es6/source/Component/XAccordion.js
@@ -0,0 +1,20 @@
+﻿// XAccordion.js
+
+class XAccordion extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.children = this.config.children || [];
+
+        this.el = {};
+    }
+
+    render() {
+
+    }
+
+}
+
+XAccordion.id = 0;
+
+XType.add('accordion', XAccordion);
