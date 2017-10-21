65ef500ecffd55f76d733fe83fc68ca7e489d760
liteng <930372551@qq.com>
2017-10-21 08:01:52 +0800

    XAccordion

--- bootstrap-es6/bootstrap-es6/source/Component/XAccordion.js
+++ bootstrap-es6/bootstrap-es6/source/Component/XAccordion.js
@@ -4,17 +4,37 @@ class XAccordion extends XObject {
 
     constructor(config) {
         super(config);
+        this.id = this.config.id || `XAccordion${XAccordion.index}`;
         this.children = this.config.children || [];
 
         this.el = {};
+        XAccordion.index++;
     }
 
     render() {
+        this.el.accordion = document.createElement('div');
+        this.el.accordion.id = this.id;
+        this.el.accordion.role = 'tablist';
+        this.container.appendChild(this.el.accordion);
 
+        this.el.card = document.createElement('div');
+        this.el.card.className = 'card-header';
+        this.el.card.role = 'tab';
+        this.el.accordion.appendChild(this.el.card);
+
+        this.el.items = [];
+
+        this.children.forEach((n, i) => {
+            this.el.items[i] = X.create(n);
+            this.el.items[i].container = this.el.card;
+            if (typeof (this.el.items[i].render) == 'function') {
+                this.el.items[i].render.call(this.el.items[i]);
+            }
+        });
     }
 
 }
 
-XAccordion.id = 0;
+XAccordion.index = 0;
 
 XType.add('accordion', XAccordion);
---
+++ bootstrap-es6/bootstrap-es6/source/Component/XAccordionItem.js
@@ -0,0 +1,22 @@
+﻿// XAccordionItem.js
+
+class XAccordionItem extends XObject {
+
+    constructor(config) {
+        super(config);
+
+        this.el = {};
+    }
+
+    render() {
+        this.el.header = document.createElement('div');
+        this.el.header.className = 'card-header';
+        this.el.header.role = 'tab';
+
+    }
+
+}
+
+XAccordionItem.index = 0;
+
+XType.add('accordionitem', XAccordionItem);
