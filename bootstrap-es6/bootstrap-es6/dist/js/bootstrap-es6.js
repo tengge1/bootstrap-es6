6dba36833e9b9c5a2599f535caf3cfcd91edb539
liteng <930372551@qq.com>
2017-10-22 19:08:50 +0800

    XFormCheck

--- bootstrap-es6/bootstrap-es6/dist/js/bootstrap-es6.js
+++ bootstrap-es6/bootstrap-es6/dist/js/bootstrap-es6.js
@@ -1892,3 +1892,34 @@ class XFormText extends XObject {
 
 XType.add('formtext', XFormText);
 
+// XFormCheck.js
+
+class XFormCheck extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.text = this.config.text || null;
+    }
+
+    render() {
+        this.el.check = document.createElement('div');
+        this.el.check.className = 'form-check';
+        this.container.appendChild(this.el.check);
+
+        this.el.label = document.createElement('label');
+        this.el.label.className = 'form-check-label';
+        this.el.check.appendChild(this.el.label);
+
+        this.el.input = document.createElement('input');
+        this.el.input.type = 'checkbox';
+        this.el.input.className = 'form-check-input';
+        this.el.label.appendChild(this.el.input);
+        if (this.text) {
+            this.el.label.append(' ' + this.text);
+        }
+    }
+
+}
+
+XType.add('formcheck', XFormCheck);
+
