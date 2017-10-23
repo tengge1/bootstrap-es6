8f2cfb916c9e51e50721f162d90dea7eddcaa15b
liteng <930372551@qq.com>
2017-10-24 07:56:04 +0800

    XJumbotron

---
+++ bootstrap-es6/bootstrap-es6/source/Component/XJumbotron.js
@@ -0,0 +1,18 @@
+﻿// XJumbotron.js
+
+class XJumbotron extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.children = this.config.children || [];
+    }
+
+    render() {
+        this.el.jumbotron = document.createElement('div');
+        this.el.jumbotron.className = 'jumbotron';
+        this.container.appendChild(this.el.jumbotron);
+    }
+
+}
+
+XType.add('jumbotron', XJumbotron);
