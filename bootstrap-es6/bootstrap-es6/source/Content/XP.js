b75b5816ba1285c3af843a143a3bb01d0a60e802
liteng <930372551@qq.com>
2017-10-24 19:54:45 +0800

    XP.js

---
+++ bootstrap-es6/bootstrap-es6/source/Content/XP.js
@@ -0,0 +1,20 @@
+﻿// XP.js
+
+class XP extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.cls = this.config.cls || null;
+    }
+
+    render() {
+        this.el.p = document.createElement('p');
+        if (this.cls) {
+            this.el.p.className = this.cls;
+        }
+        this.container.appendChild(this.el.p);
+    }
+
+}
+
+XType.add('p', XP);
