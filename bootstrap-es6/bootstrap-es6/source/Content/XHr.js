a16a19851e72457f4aca639ce49d4fbec00d0be5
liteng <930372551@qq.com>
2017-10-24 19:58:29 +0800

    XHr.js

---
+++ bootstrap-es6/bootstrap-es6/source/Content/XHr.js
@@ -0,0 +1,20 @@
+﻿// XHr.js
+
+class XHr extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.cls = this.config.cls || null;
+    }
+
+    render() {
+        this.el.hr = document.createElement('hr');
+        if (this.cls) {
+            this.el.hr.className = this.cls;
+        }
+        this.container.appendChild(this.el.hr);
+    }
+
+}
+
+XType.add('hr', XHr);
