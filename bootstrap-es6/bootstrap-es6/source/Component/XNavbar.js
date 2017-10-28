9bde818c0e6006d2b6792e5db7a538d9ebe0aa4c
liteng <930372551@qq.com>
2017-10-28 08:19:56 +0800

    XNavbarBrand

---
+++ bootstrap-es6/bootstrap-es6/source/Component/XNavbar.js
@@ -0,0 +1,25 @@
+﻿// XNavbar.js
+
+class XNavbar extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.cls = this.config.cls || null;
+        this.children = this.config.children || [];
+    }
+
+    render() {
+        this.el.nav = document.createElement('nav');
+        this.el.nav.className = 'navbar';
+        this.container.appendChild(this.el.nav);
+
+        this.children.forEach((n, i) => {
+            var obj = X.create(n);
+            obj.container = this.container;
+            obj.render.call(obj);
+        });
+    }
+
+}
+
+XType.add('navbar', XNavbar);
---
+++ bootstrap-es6/bootstrap-es6/source/Component/XNavbarBrand.js
@@ -0,0 +1,21 @@
+﻿// XNavbarBrand.js
+
+class XNavbarBrand extends XObject {
+
+    constructor(config) {
+        super(config);
+        this.text = this.config.text || 'text';
+        this.url = this.config.url || '#';
+    }
+
+    render() {
+        this.el.brand = document.createElement('a');
+        this.el.brand.className = 'navbar-brand';
+        this.el.brand.href = this.url;
+        this.el.brand.innerHTML = this.text;
+        this.container.appendChild(this.el.brand);
+    }
+
+}
+
+XType.add('navbarbrand', XNavbarBrand);
